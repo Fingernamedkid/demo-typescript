120e2e397c3db6fa53c2a86a6613e210aed930da
fingernamedkid <minhtri.trandang2005@gmail.com>
2024-11-16 20:45:35 -0500

    fix a bug

--- src/config/dotenvconfig.ts
+++ src/config/dotenvconfig.ts
@@ -4,9 +4,10 @@ import path from 'path';
 export function loadEnvConfig() {
     
     dotenv.config({ path: process.env.NODE_ENV === 'production' ? './src/config/production.env' : './src/config/testing.env' });
+    console.log("using: "+process.env.NODE_ENV)
     const config = {
         secretKey: process.env.secret || 'secret',
-        saltRound: process.env.SALTROUND || 0,
+        saltRound: process.env.SALTROUND || "0",
         admin: process.env.ADMIN || ['admin'],
 
         all: process.env.ALL || ['admin', 'user'],
