ad86bd84ffd1d39608ca9ffc5ab1c44b22480f9b
fingernamedkid <minhtri.trandang2005@gmail.com>
2024-11-16 20:47:48 -0500

    fda

--- src/config/dotenvconfig.ts
+++ src/config/dotenvconfig.ts
@@ -3,14 +3,14 @@ import path from 'path';
 
 export function loadEnvConfig() {
     
-    dotenv.config({ path: process.env.NODE_ENV === 'production' ? './src/config/production.env' : './src/config/testing.env' });
+    dotenv.config({ path: process.env.NODE_ENV === 'testing' ? './src/config/testing.env' : './src/config/production.env' });
     console.log("using: "+process.env.NODE_ENV)
     const config = {
         secretKey: process.env.secret || 'secret',
         saltRound: process.env.SALTROUND || "0",
-        admin: process.env.ADMIN || ['admin'],
+        admin: process.env.ADMIN || "admin",
 
-        all: process.env.ALL || ['admin', 'user'],
+        all: process.env.ALL || 'employee,admin',
         dbHost: process.env.DB_HOST || 'localhost',
         dbName: process.env.DB_NAME || 'test',
         dbCollection: process.env.DB_COLLECTION || 'test',
